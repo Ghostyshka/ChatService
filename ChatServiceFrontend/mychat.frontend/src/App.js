ade3cf3840b79715aa22c4df9322a1c81151a474
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-03-27 17:21:53 +0200

    configure connection for start and join method for invoke

--- ChatServiceFrontend/mychat.frontend/src/App.js
+++ ChatServiceFrontend/mychat.frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootsrtap.min.css';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Lobby from './components/Lobby';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+
+const App = () => {
+
+  const joinRoom = async (user, room) =>{
+    try{
+      const connection = new HubConnectionBuilder()
+      .withUrl("https://localhost:7049/chat")
+      .configureLogging(LogLevel.Information)
+      .build();
+
+      connection.on("ReceiveMessage", (user, message) => {
+        console.log('messege received:', message);
+      });
+
+      await connection.start();
+      await connection.invoke("JoinRoom", {user, room});
+
+    } catch(e){
+      console.log(e);
+    }
+  }
+
+  return <div className='app'>
+    <h2>MyChat</h2>
+    <hr className='line'/>
+    <Lobby joinRoom={joinRoom}/>
+  </div>
 }
 
 export default App;
